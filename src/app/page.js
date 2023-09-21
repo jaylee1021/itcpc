4fdd9647916f8ffe19ce296d4f26cdadc8721304
Jay Lee <randomdori@gmail.com>
2023-09-21 00:24:33 -0700

    feat: add main banner and css

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import styles from '../app/css/page.module.css';
+
+export default function Home() {
+  return (
+
+    <section className={styles.mainBanner}>
+      
+    </section>
+
+  );
+}
