dacabe386e5215f37c219036300e390b23d3cb88
Jay Lee <leejayjong@gmail.com>
2023-10-10 12:15:25 -0700

    feat: add sermon, new images, and fix layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,38 +2,62 @@ import Image from 'next/image';
 import styles from '../app/css/page.module.css';
 
 export default function Home() {
+
   return (
     <>
       <section className={styles.mainBanner}>
 
       </section>
-      <div style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', flexDirection: 'column', flexWrap: 'wrap', padding: '10px' }}>
+      <div className={styles.sermonTitle} >
         <h4>설교</h4>
         <h5>Sermon</h5>
       </div>
-      <section style={{ display: 'flex', maxWidth: '1522px', margin: 'auto' }}>
-        <article style={{ padding: '10px' }}>
-          <div>
-            <img src='/Korean.png' style={{ width: '741px', height: '414.95px' }} />
+      <section className={styles.videoSection}>
+        <article className={styles.padding10}>
+          <div className={styles.imageWrapper}>
+            <a href='#'><img src='/Korean.png' className={styles.articleImage} /></a>
           </div>
-          <div style={{ paddingTop: '40px', textAlign: 'center' }}>
+          <div className={styles.articleTitle} >
             <h4>소망의 하나님 God of Hope - 로마서 15:13</h4>
             <h6>2023.10.01 - 이형성 목사</h6>
           </div>
         </article>
-        <article style={{ padding: '10px' }}>
-          <div>
-            <img src='/English.png' style={{ width: '741px', height: '414.95px' }} />
+        <article className={styles.padding10}>
+          <div className={styles.imageWrapper}>
+            <a href='#'><img src='/English.png' className={styles.articleImage} /></a>
           </div>
-          <div style={{ paddingTop: '40px', textAlign: 'center' }}>
+          <div className={styles.articleTitle}>
             <h4>Go Up To Bethel - Romans 15:13</h4>
             <h6>2023.10.01 - Rev. Samuel Lee</h6>
           </div>
         </article>
       </section>
+      <section className={styles.videoSection} >
+        <div className={styles.padding10}>
+          <div className={styles.imageWrapper}>
+            <a href='#'><img src='/small_menu.png' className={styles.articleImage} /></a>
+          </div>
+        </div>
+        <div className={styles.padding10}>
+          <div className={styles.imageWrapper}>
+            <a href='#'><img src='/small_menu.png' className={styles.articleImage} /></a>
+          </div>
+        </div>
+        <div className={styles.padding10}>
+          <div className={styles.imageWrapper}>
+            <a href='#'><img src='/small_menu.png' className={styles.articleImage} /></a>
+          </div>
+        </div>
+        <div className={styles.padding10}>
+          <div className={styles.imageWrapper}>
+            <a href='#'><img src='/small_menu.png' className={styles.articleImage} /></a>
+          </div>
+        </div>
+      </section >
       <section>
-
+        another section
       </section>
+      
     </>
   );
 }
