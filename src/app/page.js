274d1d07807686a1bf594d45b155f9741787e79a
Jay Lee <leejayjong@gmail.com>
2023-10-10 14:35:45 -0700

    feat: change css file for main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,71 @@
 import Image from 'next/image';
-import styles from '../app/css/page.module.css';
+import './css/page.css';
 
 export default function Home() {
 
   return (
     <>
-      <section className={styles.mainBanner}>
+      <section className='mainBanner'>
 
       </section>
-      <div className={styles.sermonTitle} >
+      <div className='sermonTitle' >
         <h4>설교</h4>
         <h5>Sermon</h5>
       </div>
-      <section className={styles.videoSection}>
-        <article className={styles.padding10}>
-          <div className={styles.imageWrapper}>
-            <a href='#'><img src='/Korean.png' className={styles.articleImage} /></a>
+      <section className='videoSection'>
+        <article className='padding10'>
+          <div className='imageWrapper'>
+            <a href='#'><img src='/Korean.png' className='articleImage' /></a>
           </div>
-          <div className={styles.articleTitle} >
+          <div className='articleTitle' >
             <h4>소망의 하나님 God of Hope - 로마서 15:13</h4>
             <h6>2023.10.01 - 이형성 목사</h6>
           </div>
         </article>
-        <article className={styles.padding10}>
-          <div className={styles.imageWrapper}>
-            <a href='#'><img src='/English.png' className={styles.articleImage} /></a>
+        <article className='padding10'>
+          <div className='imageWrapper'>
+            <a href='#'><img src='/English.png' className='articleImage' /></a>
           </div>
-          <div className={styles.articleTitle}>
+          <div className='articleTitle'>
             <h4>Go Up To Bethel - Romans 15:13</h4>
             <h6>2023.10.01 - Rev. Samuel Lee</h6>
           </div>
         </article>
       </section>
-      <section className={styles.videoSection} >
-        <div className={styles.padding10}>
-          <div className={styles.imageWrapper}>
-            <a href='#'><img src='/small_menu.png' className={styles.articleImage} /></a>
+      <section className='videoSection' >
+        <div className='videoSection second_section'>
+          <div className='padding10'>
+            <div className='imageWrapper'>
+              <a href='#'><img src='/new_comer.png' className='articleImage' /></a>
+            </div>
+          </div>
+          <div className='padding10'>
+            <div className='imageWrapper'>
+              <a href='#'><img src='/korean_school.png' className='articleImage' /></a>
+            </div>
           </div>
         </div>
-        <div className={styles.padding10}>
-          <div className={styles.imageWrapper}>
-            <a href='#'><img src='/small_menu.png' className={styles.articleImage} /></a>
+        <div className='videoSection second_section'>
+          <div className='padding10'>
+            <div className='imageWrapper'>
+              <a href='#'><img src='/church_school.png' className='articleImage' /></a>
+            </div>
           </div>
-        </div>
-        <div className={styles.padding10}>
-          <div className={styles.imageWrapper}>
-            <a href='#'><img src='/small_menu.png' className={styles.articleImage} /></a>
-          </div>
-        </div>
-        <div className={styles.padding10}>
-          <div className={styles.imageWrapper}>
-            <a href='#'><img src='/small_menu.png' className={styles.articleImage} /></a>
+          <div className='padding10'>
+            <div className='imageWrapper'>
+              <a href='#'><img src='/direction.png' className='articleImage' /></a>
+            </div>
           </div>
         </div>
       </section >
-      <section>
-        another section
+      <section className='new_comer'>
+        <div className='new_comer_content'>
+          New Comer section (image goes here)
+        </div>
+
+
       </section>
-      
+
     </>
   );
 }
