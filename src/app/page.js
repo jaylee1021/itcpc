e7c2bfe6745bfae1de3629147d2fe1ce9fe7b1cd
Jay Lee <randomdori@gmail.com>
2023-10-10 00:01:19 -0700

    feat: add sermon section

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,37 @@ import styles from '../app/css/page.module.css';
 
 export default function Home() {
   return (
+    <>
+      <section className={styles.mainBanner}>
 
-    <section className={styles.mainBanner}>
+      </section>
+      <div style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', flexDirection: 'column', flexWrap: 'wrap', padding: '10px' }}>
+        <h4>설교</h4>
+        <h5>Sermon</h5>
+      </div>
+      <section style={{ display: 'flex', maxWidth: '1522px', margin: 'auto' }}>
+        <article style={{ padding: '10px' }}>
+          <div>
+            <img src='/Korean.png' style={{ width: '741px', height: '414.95px' }} />
+          </div>
+          <div style={{ paddingTop: '40px', textAlign: 'center' }}>
+            <h4>소망의 하나님 God of Hope - 로마서 15:13</h4>
+            <h6>2023.10.01 - 이형성 목사</h6>
+          </div>
+        </article>
+        <article style={{ padding: '10px' }}>
+          <div>
+            <img src='/English.png' style={{ width: '741px', height: '414.95px' }} />
+          </div>
+          <div style={{ paddingTop: '40px', textAlign: 'center' }}>
+            <h4>Go Up To Bethel - Romans 15:13</h4>
+            <h6>2023.10.01 - Rev. Samuel Lee</h6>
+          </div>
+        </article>
+      </section>
+      <section>
 
-    </section>
-
+      </section>
+    </>
   );
 }
