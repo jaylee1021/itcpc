c6d3a65550d74918985dc3431e9a68417b90f52a
Jay Lee <leejayjong@gmail.com>
2023-10-10 14:35:56 -0700

    add footer to layout page

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
 
     </html>
