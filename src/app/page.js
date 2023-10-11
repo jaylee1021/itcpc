ea9af8988df7a22f78ba95a7b5045138cdd4a356
Jay Lee <leejayjong@gmail.com>
2023-10-11 15:24:26 -0700

    add what's new section on main

--- src/app/page.js
+++ src/app/page.js
@@ -60,16 +60,42 @@ export default function Home() {
       </section >
       <section className='new_comer'>
         <div className='new_comer_content'>
-          New Comer section (image goes here)
+          <h1>새가족 안내</h1>
+          <p>타코마중앙장로교회를 방문해 주신 여러분들을 주님의 이름으로 환영합니다. </p>
+          <p>타코마중앙장로교회에서 함께 신앙생활을 하시기 원하시는 분들께서는 매 주일 교회 본당 안내석에 위치한 새가족부 봉사자들의 안내를 받아 등록 해 주시기 바랍니다.</p>
+          <p>새가복으로 등록 하신 분들은 교회 소개와 안내, 소그룹안내(구역) 밑 새가족 교육에 참여하시면서 교회 생활에 필요한 도움을 받으시게 됩니다.</p>
         </div>
       </section>
       <div className='title'>
         <h4>주일스케치</h4>
         <h5>What's New</h5>
       </div>
-      <section>
-        
-      </section>
+      <section className='videoSection'>
+        <div className='whats_new_image_wrapper'>
+          <div className=''>
+            <a href='#'><img src='/2023_vbs.png' className='whats_new_image' /></a>
+          </div>
+          <div className='more-info'>
+            <p>2023 VBS</p>
+          </div>
+        </div>
+        <div className='whats_new_image_wrapper'>
+          <div className=''>
+            <a href='#'><img src='/2023_picnic.png' className='whats_new_image' /></a>
+          </div>
+          <div className='more-info'>
+            <p>2023 전교인 피크닉</p>
+          </div>
+        </div>
+        <div className='whats_new_image_wrapper'>
+          <div className=''>
+            <a href='#'><img src='/2023_appointment.png' className='whats_new_image' /></a>
+          </div>
+          <div className='more-info'>
+            <p>2023 신년 임직식</p>
+          </div>
+        </div >
+      </section >
     </>
   );
 }
