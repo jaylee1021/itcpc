bc2e9e29dd08f3dd8ab8f4d1bcc888e5cb100c71
Jay Lee <randomdori@gmail.com>
2023-10-13 00:00:37 -0700

    feat: change footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,66 +1,105 @@
 'use client';
 
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../app/css/globals.css';
 import '../app/css/footer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 export default function Footer() {
+    const form = useRef();
+    const [message, setMessage] = useState('');
+    const [user_name, setUserName] = useState('');
+    const [user_email, setUserEmail] = useState('');
 
+    const handleUserName = (e) => {
+        setUserName(e.target.value);
+    };
+
+    const handleUserEmail = (e) => {
+        setUserEmail(e.target.value);
+    };
+
+    const handleMessage = (e) => {
+        setMessage(e.target.value);
+    };
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, form.current, process.env.NEXT_PUBLIC_PUBLIC_KEY)
+            .then((result) => {
+                alert('메세지가 성공적으로 보내졌습니다. 최대한 빨리 연락드리겠습니다. 감사합니다.');
+                setMessage('');
+                setUserName('');
+                setUserEmail('');
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
 
     return (
         <footer>
-            <nav className="navbar sticky-bottom footer-style" >
+            <nav className="navbar sticky-bottom footer-style" style={{ display: 'flex', justifyContent: 'center' }}>
                 <div className='footer-group'>
                     <div className='footer-section'>
+                        <div className='logo-wrap'>
+                            <img src='/itcpc-logo-dark.png' className='footer-logo' />
+                        </div>
+                        <br />
+                        <br />
+                        <br />
                         <p>
                             253-589-8990
                             <br />
-                        </p>
-                        <p>
                             <a href='mailto:office@itcpc.org'>office@itcpc.org</a>
                         </p>
+                        <br />
                         <p>
-                            10002 59th Ave SW
-                            <br />
-                            Lakewood, WA 98499
+                            10002 59th Ave SW, Lakewood, WA 98499
                         </p>
+                        <br />
+                        <br />
                         <p>
-                            <a href='https://www.facebook.com/itgroup.tcpc' target='_blank'>
-                                <img src='/facebook.png' className='social-icon' />
-                            </a>
-
                             <a href='https://www.youtube.com/@TCPCTV' target='_blank'>
-                                <img src='/youtube.png' className='social-icon' />
+                                <img src='/youtube.webp' className='social-icon' />
+                            </a>
+                            <a href='https://www.facebook.com/itgroup.tcpc' target='_blank'>
+                                <img src='/facebook.webp' className='social-icon' />
                             </a>
                         </p>
                         <p>
-                            Copyright(c) 2023.
-                            <br />
-                            Tacoma Central Presbyterian Church.
+                            ©2023 by Tacoma Central Presbyterian Church.
                             <br />
                             All rights reserved.
                         </p>
                     </div>
                     <div className='footer-section'>
-                        <p>
-                            About
-                        </p>
+                        <form className='footer-question' ref={form} onSubmit={sendEmail}>
+                            <p>
+                                질문이 있으신가요?
+                            </p>
+                            <div>
+                                <input type='text' name='user_name' value={user_name} onChange={handleUserName} className='footer-input' placeholder='이름' required />
+                            </div>
+                            <br />
+                            <div>
+                                <input type='email' name='user_email' value={user_email} onChange={handleUserEmail} className='footer-input' placeholder='이메일' required />
+                            </div>
+                            <br />
+                            <div>
+                                <textarea className='footer-message' name='message' value={message} onChange={handleMessage} placeholder='메세지' required />
+                            </div>
+
+                            <div className='footer-button-to-right'>
+                                <input type='submit' className='footer-button' value={'보내기'} />
+                            </div>
+                        </form>
                     </div>
-                    <div className='footer-section'>
-                    <p>
-                            Sermon
-                        </p>
-                    </div>
-                    <div className='footer-section'>
-                    <p>
-                            교회학교
-                        </p>
-                    </div>
+
                 </div>
             </nav >
         </footer >
