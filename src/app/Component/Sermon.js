e76641950fb0a785143868aac255d7c9598e9018
Jay Lee <randomdori@gmail.com>
2023-10-13 00:01:44 -0700

    feat: add sermon component and page

---
+++ src/app/Component/Sermon.js
@@ -0,0 +1,14 @@
+'use clien';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+export default function Sermon() {
+
+
+    return (
+        <>
+
+        </>
+    );
+}
