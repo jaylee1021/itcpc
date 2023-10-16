39a44ee57a43c283f5ee891fd733f0841513be34
Jay Lee <leejayjong@gmail.com>
2023-10-16 16:23:12 -0700

    change link style

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -22,55 +22,49 @@ export default function Navbar() {
             <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">교회소개</a>
                 <ul className="dropdown-menu">
-                    <li><a href='#'><MenuItem>환영인사</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>새가족</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>교회비전</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>교회연헉</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>예배안내/약도</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>섬기는분들</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>환영인사</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>새가족</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>교회비전</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>교회연헉</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>예배안내/약도</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>섬기는분들</MenuItem></a></li>
                 </ul>
             </li>
             <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">설교와 찬양</a>
                 <ul className="dropdown-menu">
-                    <li><a href='#'><MenuItem>설교말씀</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>찬양</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>설교말씀</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>찬양</MenuItem></a></li>
                 </ul>
             </li>
             <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">교회학교</a>
-                <ul className="dropdown-menu">
-                    <li><a href='#'><MenuItem>영유아부 Promise Land</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>유년부 Team K5</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>중고등부 Matus United</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>교육 계시판</MenuItem></a></li>
-                </ul>
+                <a className="nav-link" href="./education" role="button" data-bs-toggle="dropdown" aria-expanded="false">교회학교</a>
             </li>
             <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">선교와 공동체</a>
                 <ul className="dropdown-menu">
-                    <li><a href='#'><MenuItem>선교회/구역</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>세계선교</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>평신도훈련</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>타코마한국학교</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>늘푸른대학</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>선교회/구역</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>세계선교</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>평신도훈련</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>타코마한국학교</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>늘푸른대학</MenuItem></a></li>
                 </ul>
             </li>
             <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">교회소식</a>
                 <ul className="dropdown-menu">
-                    <li><a href='#'><MenuItem>주보</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>주일스케치</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>자유게시판</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>캘린더</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>주보</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>주일스케치</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>자유게시판</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>캘린더</MenuItem></a></li>
                 </ul>
             </li>
             <li className="nav-item">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">English Ministry</a>
                 <ul className="dropdown-menu">
                     <li><a href='http://www.owtcpc.org' target='_blank'><MenuItem>Open Worship(열린예배)</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>English Ministry</MenuItem></a></li>
-                    <li><a href='#'><MenuItem>EM 게시판 EM Board</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>English Ministry</MenuItem></a></li>
+                    <li><a href='#' className='menu_link'><MenuItem>EM 게시판 EM Board</MenuItem></a></li>
                 </ul>
             </li>
             <hr />
@@ -82,12 +76,12 @@ export default function Navbar() {
         <li className="nav-item dropdown dropdowns">
             <a className="nav-link" style={{ fontWeight: 'bold' }} href="./about" >교회소개</a>
             <ul className="dropdown-menu menus">
-                <li><a href='#'><MenuItem>환영인사</MenuItem></a></li>
-                <li><a href='#'><MenuItem>새가족</MenuItem></a></li>
-                <li><a href='#'><MenuItem>교회비전</MenuItem></a></li>
-                <li><a href='#'><MenuItem>교회연헉</MenuItem></a></li>
-                <li><a href='#'><MenuItem>예배안내/약도</MenuItem></a></li>
-                <li><a href='#'><MenuItem>섬기는분들</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>환영인사</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>새가족</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>교회비전</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>교회연헉</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>예배안내/약도</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>섬기는분들</MenuItem></a></li>
             </ul>
         </li>
         <li>
@@ -96,21 +90,15 @@ export default function Navbar() {
         <li className="nav-item dropdown dropdowns">
             <a className="nav-link" style={{ fontWeight: 'bold' }} href="#">설교와 찬양</a>
             <ul className="dropdown-menu menus">
-                <li><a href='#'><MenuItem>설교말씀</MenuItem></a></li>
-                <li><a href='#'><MenuItem>찬양</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>설교말씀</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>찬양</MenuItem></a></li>
             </ul>
         </li>
         <li>
             <a className="nav-link" href="#">|</a>
         </li>
         <li className="nav-item dropdown dropdowns">
-            <a className="nav-link" style={{ fontWeight: 'bold' }} href="#">교회학교</a>
-            <ul className="dropdown-menu menus">
-                <li><a href='#'><MenuItem>영유아부 Promise Land</MenuItem></a></li>
-                <li><a href='#'><MenuItem>유년부 Team K5</MenuItem></a></li>
-                <li><a href='#'><MenuItem>중고등부 Matus United</MenuItem></a></li>
-                <li><a href='#'><MenuItem>교육 계시판</MenuItem></a></li>
-            </ul>
+            <a className="nav-link" style={{ fontWeight: 'bold' }} href="./education">교회학교</a>
         </li>
         <li>
             <a className="nav-link" href="#">|</a>
@@ -118,11 +106,11 @@ export default function Navbar() {
         <li className="nav-item dropdown dropdowns">
             <a className="nav-link" style={{ fontWeight: 'bold' }} href="#">선교와 공동체</a>
             <ul className="dropdown-menu menus">
-                <li><a href='#'><MenuItem>선교회/구역</MenuItem></a></li>
-                <li><a href='#'><MenuItem>세계선교</MenuItem></a></li>
-                <li><a href='#'><MenuItem>평신도훈련</MenuItem></a></li>
-                <li><a href='#'><MenuItem>타코마한국학교</MenuItem></a></li>
-                <li><a href='#'><MenuItem>늘푸른대학</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>선교회/구역</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>세계선교</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>평신도훈련</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>타코마한국학교</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>늘푸른대학</MenuItem></a></li>
             </ul>
         </li>
         <li>
@@ -131,10 +119,10 @@ export default function Navbar() {
         <li className="nav-item dropdown dropdowns">
             <a className="nav-link" style={{ fontWeight: 'bold' }} href="#">교회소식</a>
             <ul className="dropdown-menu menus">
-                <li><a href='#'><MenuItem>주보</MenuItem></a></li>
-                <li><a href='#'><MenuItem>주일스케치</MenuItem></a></li>
-                <li><a href='#'><MenuItem>자유게시판</MenuItem></a></li>
-                <li><a href='#'><MenuItem>캘린더</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>주보</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>주일스케치</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>자유게시판</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>캘린더</MenuItem></a></li>
             </ul>
         </li>
         <li>
@@ -143,18 +131,17 @@ export default function Navbar() {
 
         <li className="nav-item dropdown dropdowns">
             <a className="nav-link dropwodn-toggle" style={{ fontWeight: 'bold' }} href="#">English Ministry</a>
-
             <ul className="dropdown-menu menus drop-to-left" >
-                <li><a href='http://www.owtcpc.org' target='_blank'><MenuItem>Open Worship(열린예배)</MenuItem></a></li>
-                <li><a href='#'><MenuItem>English Ministry</MenuItem></a></li>
-                <li><a href='#'><MenuItem>EM 게시판 EM Board</MenuItem></a></li>
+                <li><a href='http://www.owtcpc.org' target='_blank' className='menu_link'><MenuItem>Open Worship(열린예배)</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>English Ministry</MenuItem></a></li>
+                <li><a href='#' className='menu_link'><MenuItem>EM 게시판 EM Board</MenuItem></a></li>
             </ul>
         </li>
     </ul>);
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg sticky-top" style={{ position: 'sticky', top: '0' }}>
+            <nav className="navbar navbar-expand-lg sticky-top" >
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/"><img src='/itcpc-logo.png' alt="Logo" /></a>
                     <button className="navbar-toggler" onClick={toggleMenu} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
