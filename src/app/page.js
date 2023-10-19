7b9ccccc57931bd48eb754039d9ff15c7c6a30c4
Jay Lee <leejayjong@gmail.com>
2023-10-18 23:29:32 -0700

    add accordion effect on news

--- src/app/page.js
+++ src/app/page.js
@@ -119,7 +119,7 @@ export default function Home() {
         <div className='videoSection second_section'>
           <div className='padding10'>
             <div className='imageWrapper'>
-              <a href='#'><img src='/new_comer.png' className='articleImage' /></a>
+              <a href='./newComer'><img src='/new_comer.png' className='articleImage' /></a>
             </div>
           </div>
           <div className='padding10'>
@@ -136,7 +136,7 @@ export default function Home() {
           </div>
           <div className='padding10'>
             <div className='imageWrapper'>
-              <a href='#'><img src='/direction.png' className='articleImage' /></a>
+              <a href='./serviceAndDirection'><img src='/direction.png' className='articleImage' /></a>
             </div>
           </div>
         </div>
@@ -169,47 +169,56 @@ export default function Home() {
       </section>
       <br />
       <br />
-      <br />
-      <section className='new_comer'>
-        <div className='new_comer_content'>
-          <h1>새가족 안내</h1>
-          <p>타코마중앙장로교회를 방문해 주신 여러분들을 주님의 이름으로 환영합니다. </p>
-          <p>타코마중앙장로교회에서 함께 신앙생활을 하시기 원하시는 분들께서는 매 주일 교회 본당 안내석에 위치한 새가족부 봉사자들의 안내를 받아 등록 해 주시기 바랍니다.</p>
-          <p>새가복으로 등록 하신 분들은 교회 소개와 안내, 소그룹안내(구역) 밑 새가족 교육에 참여하시면서 교회 생활에 필요한 도움을 받으시게 됩니다.</p>
-        </div>
-      </section>
-      <br />
-      <br />
       <div className='title'>
         <p className='title-style'>소식</p>
         <p className='subtitle-style'>News</p>
       </div>
       <section className='videoSection'>
-        <div className='whats_new_image_wrapper'>
-          <div className=''>
-            <a href='#'><img src='/2023_vbs.png' className='whats_new_image' /></a>
-          </div>
-          <div className='more-info'>
-            <p>2023 VBS</p>
-          </div>
+        <div className='news_body'>
+          <div className='news_wrapper'>
+            <div className='news_image'>
+              <div className='news_description'>
+                <h2>예배안내</h2>
+                <button className='news_sub'>더보기</button>
+              </div>
+            </div>
+            <div className='news_image'>
+              <div className='news_description'>
+                <h2>부흥회</h2>
+                <button className='news_sub'>더보기</button>
+              </div>
+            </div>
+            <div className='news_image' >
+              <div className='news_description'>
+                <h2>피크닉</h2>
+                <button className='news_sub'>더보기</button>
+              </div>
+            </div>
+            <div className='news_image'>
+              <div className='news_description'>
+                <h2>임직식</h2>
+                <button className='news_sub'>더보기</button>
+              </div>
+            </div>
+          </div >
         </div>
-        <div className='whats_new_image_wrapper'>
-          <div className=''>
-            <a href='#'><img src='/2023_picnic.png' className='whats_new_image' /></a>
-          </div>
-          <div className='more-info'>
-            <p>2023 전교인 피크닉</p>
-          </div>
-        </div>
-        <div className='whats_new_image_wrapper'>
-          <div className=''>
-            <a href='#'><img src='/2023_appointment.png' className='whats_new_image' /></a>
-          </div>
-          <div className='more-info'>
-            <p>2023 신년 임직식</p>
-          </div>
-        </div >
       </section >
+      <br />
+      <br />
+      <br />
+      <div className='title'>
+        <p className='title-style'>새가족 안내</p>
+        <p className='subtitle-style'>New Comer</p>
+      </div>
+      <section className='new_comer'>
+        <div className='new_comer_content'>
+
+          <p>타코마중앙장로교회를 방문해 주신 여러분들을 주님의 이름으로 환영합니다. </p>
+          <p>타코마중앙장로교회에서 함께 신앙생활을 하시기 원하시는 분들께서는 매 주일 교회 본당 안내석에 위치한 새가족부 봉사자들의 안내를 받아 등록 해 주시기 바랍니다.</p>
+          <p>새가복으로 등록 하신 분들은 교회 소개와 안내, 소그룹안내(구역) 밑 새가족 교육에 참여하시면서 교회 생활에 필요한 도움을 받으시게 됩니다.</p>
+        </div>
+      </section>
+
       <br />
       <br />
       <div className='title'>
