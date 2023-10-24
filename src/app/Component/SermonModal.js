568c2e3dc20211c9c76d3a604a304840a396b86b
Jay Lee <randomdori@gmail.com>
2023-10-24 01:22:50 -0700

    feat: add sermonModal.js

---
+++ src/app/Component/SermonModal.js
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import '../css/page.css';
+import '../css/sermons.css';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    maxHeight: '100vh',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '100%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    padding: '32px'
+};
+// const OVERLAY_STYLE = {
+//     position: "fixed",
+//     display: "flex",
+//     justifyContent: "center",
+//     top: "0",
+//     left: "0",
+//     width: "100%",
+//     height: "100%",
+//     backgroundColor: "rgba(0,0,0, .8)",
+//     zIndex: "1000",
+//     overflowY: "auto"
+// };
+
+export default function SermonModal({ sermon }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <div onClick={handleOpen} className='sermon_info sermon_info_modal' style={{ fontSize: '2rem', padding: '0' }}>
+                {sermon.title} - {sermon.passage}
+            </div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    {/* <div style={OVERLAY_STYLE}> */}
+                    <Box sx={style}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2" style={{ overflow: 'auto' }}>
+                            {/* <img src={image} style={{ maxHeight: '90vh' }} alt="..." /> */}
+                            sermon modal
+                        </Typography>
+                        {/* <Typography id="transition-modal-description" sx={{ mt: 2 }}> */}
+                        {/* Your description */}
+                        {/* </Typography> */}
+                    </Box>
+                    {/* </div> */}
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
