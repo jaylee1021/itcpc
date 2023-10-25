e9296feb2a5c406a409ae089b483626d40af3243
Jay Lee <leejayjong@gmail.com>
2023-10-25 14:40:15 -0700

    change link to image

--- src/app/Component/GetAllSermon.js
+++ src/app/Component/GetAllSermon.js
@@ -37,16 +37,16 @@ export default function GetAllSermon() {
             {currentPosts.map((sermon) => (
                 <div className="component_sermon_section" key={sermon._id}>
                     <div className="sermon_snap">
-                        <img src={sermon.snap} className='sermon_snap' />
+                        <SermonModal sermon={sermon} />
                     </div>
                     <div className="sermon_info" >
                         <h3>
-                            <SermonModal sermon={sermon} />
+                            {sermon.title} - {sermon.passage}
                         </h3>
                         <p className='date_time'>
-                            {sermon.date.split('T')[0]} @ {sermon.session === 'First' ? '8:00 AM 1부' : null ||
-                                sermon.session === 'Second' ? '09:30 AM 2부' : null ||
-                                    sermon.session === 'Third' ? '11:00 AM 3부' : null} - {sermon.preacher} 목사
+                            {sermon.date.split('T')[0]} @ {sermon.session === '1부' ? '8:00 AM 1부' : null ||
+                                sermon.session === '2부' ? '09:30 AM 2부' : null ||
+                                    sermon.session === '3부' ? '11:00 AM 3부' : null} - {sermon.preacher} 목사
                         </p>
                     </div>
                 </div>))}
