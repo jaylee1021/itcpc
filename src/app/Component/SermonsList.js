a5db99d6437b3e88b5c17220ea4eeec32063fb08
Jay Lee <randomdori@gmail.com>
2023-10-29 16:44:53 -0700

    add edge case on sermonlist

--- src/app/Component/SermonsList.js
+++ src/app/Component/SermonsList.js
@@ -4,6 +4,12 @@ import SermonModal from "./SermonModal";
 
 export default function SermonsList({ sermon }) {
 
+    let emPreacher = '';
+    if (sermon.preacher.includes('Danny Kim') || sermon.preacher.includes('David Rho')) {
+        emPreacher = 'Pastor ' + sermon.preacher;
+    } else {
+        emPreacher = 'Rev. ' + sermon.preacher;
+    }
     return (
         <>
             <div className="component_sermon_section" key={sermon._id}>
@@ -16,7 +22,7 @@ export default function SermonsList({ sermon }) {
                     </h3>
                     <p className='date_time'>
                         {sermon.date.split('T')[0]} @ {sermon.session === '1부' ? '8:00 AM 1부 - ' + sermon.preacher + '목사' : null ||
-                            sermon.session === '2부' ? '9:30 AM 2부 - ' + 'Rev. ' + sermon.preacher : null ||
+                            sermon.session === '2부' ? '9:30 AM 2부 - ' + emPreacher : null ||
                                 sermon.session === '3부' ? '11:00 AM 3부 - ' + sermon.preacher + '목사' : null}
                     </p>
                 </div>
