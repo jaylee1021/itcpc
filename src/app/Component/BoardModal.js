e300b79a41996f0eee7fe312f61becb92147dd56
Jay Lee <leejayjong@gmail.com>
2023-11-03 15:19:10 -0700

    add board modal

---
+++ src/app/Component/BoardModal.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import '../css/page.css';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    maxHeight: '100vh',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '100%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    padding: '32px'
+};
+
+export default function BoardModal({ image }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <div onClick={handleOpen} style={{ color: 'black', padding: '0', cursor: 'pointer' }}>
+                {image.title}
+            </div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2" style={{ overflow: 'auto' }}>
+                            <img src={image.url} style={{ maxHeight: '90vh' }} alt="..." />
+                        </Typography>
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
