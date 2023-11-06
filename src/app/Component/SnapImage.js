b5dfab2cdd1f465f940b7bfe34e5f85d55a71aca
Jay Lee <leejayjong@gmail.com>
2023-11-06 14:37:24 -0800

    edit snapimage.js

--- src/app/Component/SnapImage.js
+++ src/app/Component/SnapImage.js
@@ -3,37 +3,36 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { LoadingLine } from './Loading';
 
-export default function SnapImages({ snap }) {
-
-    const [snapUrl, setSnapUrl] = useState('');
+export default function SnapImage({ snap }) {
+    console.log(snap);
     const [isLoading, setIsLoading] = useState(true);
+    const [imageUrl, setImageUrl] = useState('');
     const [imageUploaded, setImageUploaded] = useState(false);
 
-    useEffect(() => {
-        if (!imageUploaded) {
-            const formData = new FormData();
-            formData.append('file', snap);
-            formData.append('upload_preset', 'itcpc_files');
-            fetch('https://api.cloudinary.com/v1_1/instaversecloud/image/upload/', {
-                method: 'POST',
-                body: formData,
+
+    if (!imageUploaded) {
+        const formData = new FormData();
+        formData.append('file', snap);
+        formData.append('upload_preset', 'itcpc_files');
+        fetch('https://api.cloudinary.com/v1_1/instaversecloud/image/upload/', {
+            method: 'POST',
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setImageUrl(data.secure_url);
+                console.log(data.secure_url);
+                setIsLoading(false);
+                setImageUploaded(true);
             })
-                .then((response) => response.json())
-                .then((data) => {
-                    setSnapUrl(data.secure_url);
-                    console.log(data.secure_url);
-                    setImageUploaded(true);
-                    setIsLoading(false);
-                })
-                .catch((error) => console.log('Error', error));
-        };
-    }, [snap, imageUploaded]);
+            .catch((error) => console.log('Error', error));
+    };
 
     if (isLoading) return (<LoadingLine />);
 
     return (
         <>
-            {snapUrl}
+            {imageUrl}
         </>
     );
 }
