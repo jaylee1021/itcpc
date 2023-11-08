1230840118ef90a1aad06c0a2fb615e2a010148e
Jay Lee <randomdori@gmail.com>
2023-11-08 00:57:59 -0800

    edit style

--- src/app/Component/GetHistory.js
+++ src/app/Component/GetHistory.js
@@ -3,28 +3,31 @@ import React, { useEffect, useState } from 'react';
 import '../css/page.css';
 import '../css/history.css';
 import axios from 'axios';
+import { LoadingCircle } from './Loading';
 
 export default function GetHistory({ order }) {
 
     const [histories, setHistories] = useState([]);
-
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/histories/${order}`)
             .then(response => {
                 if (response.status === 200) {
                     console.log('history', histories);
                     setHistories(response.data.histories);
+                    setIsLoading(true);
                 }
             })
             .catch(error => console.log('===> Error fetching histories', error));
     }, [order]);
 
+    if (!isLoading) return (<LoadingCircle />);
 
     return (
         <>
             {histories.map((history, index) => (
                 <div key={index} className='history_article'>
-                    <div className='history_article_date' style={{ width: '15%' }}>
+                    <div className='history_article_date' >
                         <p>{history.date}</p>
                     </div>
                     <div className='history_article_description'>
--- src/app/history/page.js
+++ src/app/history/page.js
@@ -19,6 +19,8 @@ export default function History() {
             })
             .catch(error => console.log('===> Error fetching histories', error));
     });
+
+
     return (
         <>
             <title>타코마중앙장로교회</title>
@@ -32,19 +34,60 @@ export default function History() {
                 <p className='subtitle-style'>Church History</p>
             </div>
             <br />
-            <section style={{ textAlign: 'center', maxWidth: '1440px', margin: 'auto' }}>
+            <section style={{ textAlign: 'center', maxWidth: '950px', margin: 'auto' }}>
                 <h4>
                     타코마 중앙 장로 교회는 1972년 창립되어 1973년 초대담임 목사님이 청빙 된후 지난 50여년간
+                    <br />
                     미주 서북미의 한인 교민들에게 복음을 전하고 신앙 성장을 위해 주님의 뚯을 좇아 달려왔습니다.
                 </h4>
             </section>
-            <section>
-                <a href='./history#order_1'> 창립초기 1972 - 1976</a>
+            <section >
+                <div style={{ display: 'flex', justifyContent: 'space-evenly', maxWidth: '950px', margin: 'auto', textAlign: 'center' }}>
+                    <a href='./history#order_1'> 창립초기 <br /> 1972 - 1976</a>
+                    <a href='./history#order_2'> 건축 및 성장기  <br /> 1977 - 1997</a>
+                    <a href='./history#order_3'> 선교 지향기  <br /> 1998 - 2003</a>
+                    <a href='./history#order_4'> 선교 사역 집중기  <br /> 2004 - 현재</a>
+                </div>
+
             </section>
             <br />
-            <section id='order_1'>
-                <GetHistory order={1} />
-            </section>
+            <div style={{ maxWidth: '950px', margin: 'auto' }}>
+                <div id='order_1'>
+                    <p>
+                        창립초기 1972 - 1976
+                    </p>
+                </div>
+                <section>
+                    <GetHistory order={1} />
+                </section>
+                <br />
+                <div id='order_2'>
+                    <p>
+                        건축 및 성장기 1977 - 1997
+                    </p>
+                </div>
+                <section >
+                    <GetHistory order={2} />
+                </section>
+                <br />
+                <div id='order_3'>
+                    <p>
+                        선교 지향기 1998 - 2003
+                    </p>
+                </div>
+                <section >
+                    <GetHistory order={3} />
+                </section>
+                <br />
+                <div id='order_4'>
+                    <p>
+                        선교 사역 집중기 2004 - 현재
+                    </p>
+                </div>
+                <section >
+                    <GetHistory order={4} />
+                </section>
+            </div>
         </>
     );
 };
