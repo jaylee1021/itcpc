dd68d1ea338a9c1ab34c227592ebba27b7adaae5
Jay Lee <randomdori@gmail.com>
2023-11-08 22:54:35 -0800

    fix styling

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -29,23 +29,27 @@ export default function History() {
             <section style={{ textAlign: 'center', maxWidth: '950px', margin: 'auto' }}>
                 <h4>
                     타코마 중앙 장로 교회는 1972년 창립되어 1973년 초대담임 목사님이 청빙 된후 지난 50여년간
-                    <br />
                     미주 서북미의 한인 교민들에게 복음을 전하고 신앙 성장을 위해 주님의 뚯을 좇아 달려왔습니다.
                 </h4>
             </section>
             <br />
             <div className='top'>
                 <div className='top_background'>
-                    <section >
-                        <div className='history_buttons_section'>
-                            <a href='./history#order_1'><button className='history_buttons'>창립초기 <br /> 1972 - 1976</button></a>
-                            <a href='./history#order_2'><button className='history_buttons'>건축 및 성장기 <br /> 1977 - 1997</button></a>
-                            <a href='./history#order_3'><button className='history_buttons'>선교 지향기 <br /> 1998 - 2003</button></a>
-                            <a href='./history#order_4'><button className='history_buttons'>선교 사역 집중기  <br /> 2004 - 현재</button></a>
+                    <section className='history_buttons_section'>
+                        <div className='history_buttons_section button_media'>
+                            <div className='two_buttons'>
+                                <a href='./history#order_1'><button className='history_buttons'>창립초기 <br /> 1972 - 1976</button></a>
+                                <a href='./history#order_2'><button className='history_buttons'>건축 및 성장기 <br /> 1977 - 1997</button></a>
+                            </div>
+                            <div className='two_buttons'>
+                                <a href='./history#order_3'><button className='history_buttons'>선교 지향기 <br /> 1998 - 2003</button></a>
+                                <a href='./history#order_4'><button className='history_buttons'>선교 사역 집중기  <br /> 2004 - 현재</button></a>
+                            </div>
                         </div>
                     </section>
                     <br />
                     <div style={{ maxWidth: '950px', margin: 'auto' }}>
+                        <hr />
                         <div id='order_1'>
                             <p className='history_title'>
                                 창립초기 1972 - 1976
@@ -54,7 +58,7 @@ export default function History() {
                         <section>
                             <GetHistory order={1} />
                         </section>
-                        <br />
+                        <hr />
                         <div id='order_2'>
                             <p className='history_title'>
                                 건축 및 성장기 1977 - 1997
@@ -63,18 +67,16 @@ export default function History() {
                         <section >
                             <GetHistory order={2} />
                         </section>
-                        <br />
+                        <hr />
                         <div id='order_3' >
                             <p className='history_title'>
                                 선교 지향기 1998 - 2003
                             </p>
-                            <hr />
-
                         </div>
                         <section >
                             <GetHistory order={3} />
                         </section>
-                        <br />
+                        <hr />
                         <div id='order_4'>
                             <p className='history_title'>
                                 선교 사역 집중기 2004 - 현재
