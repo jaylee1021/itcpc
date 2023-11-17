11cb270681d14f34f1678f93894090cd48f70a65
Jay Lee <leejayjong@gmail.com>
2023-11-17 10:40:36 -0800

    remove get last sermon

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import { LoadingSpinningBubble } from './Component/Loading';
 import axios from 'axios';
 import PhotoModal from './Component/PhotoModal';
 import GetLastSermon from './Component/GetLastSermon';
-import GetLastSermonDate from './Component/GetLastSermonDate';
 import Link from 'next/link';
 import './css/page.css';
 import './css/gallery.css';
@@ -91,32 +90,10 @@ export default function Home() {
         <p className='title-style'>주일 예배</p>
         <p className='subtitle-style'>Sunday Worship</p>
       </div>
-
       <section className='videoSection'>
-        <article className='sermon_padding10'>
-          <div className='imageWrapper'>
-            <GetLastSermon sermonSession='1부' />
-          </div>
-          <div className='articleSub'>
-            <GetLastSermonDate sermonSession='1부' /> 1부 주일예배와 성가대 찬양
-          </div>
-        </article>
-        <article className='sermon_padding10'>
-          <div className='imageWrapper'>
-            <GetLastSermon sermonSession='2부' />
-          </div>
-          <div className='articleSub'>
-            <GetLastSermonDate sermonSession='2부' /> 2부 주일 영어예배
-          </div>
-        </article>
-        <article className='sermon_padding10'>
-          <div className='imageWrapper'>
-            <GetLastSermon sermonSession='3부' />
-          </div>
-          <div className='articleSub'>
-            <GetLastSermonDate sermonSession='3부' /> 3부 주일예배와 실로암 찬양대
-          </div>
-        </article>
+        <GetLastSermon sermonSession='1부' />
+        <GetLastSermon sermonSession='2부' />
+        <GetLastSermon sermonSession='3부' />
       </section>
       <br />
       <div className='title'>
@@ -153,10 +130,7 @@ export default function Home() {
           </div >
         </div>
       </section >
-      <br />
-      <br />
-      <br />
-      <section className='videoSection' >
+      <section className='videoSection four_button_margin'>
         <div className='videoSection second_section'>
           <div className='padding10'>
             <div className='imageWrapper'>
@@ -182,28 +156,23 @@ export default function Home() {
           </div>
         </div>
       </section >
-      <br />
-      <br />
       <div className='title'>
         <p className='title-style'>새가족 안내</p>
         <p className='subtitle-style'>New Comer</p>
       </div>
       <section className='new_comer'>
         <div className='new_comer_content'>
-
           <p>타코마중앙장로교회를 방문해 주신 여러분들을 주님의 이름으로 환영합니다. </p>
           <p>타코마중앙장로교회에서 함께 신앙생활을 하시기 원하시는 분들께서는 매 주일 교회 본당 안내석에 위치한 새가족부 봉사자들의 안내를 받아 등록 해 주시기 바랍니다.</p>
           <p>새가족으로 등록 하신 분들은 교회 소개와 안내, 소그룹안내(구역) 밑 새가족 교육에 참여하시면서 교회 생활에 필요한 도움을 받으시게 됩니다.</p>
         </div>
       </section>
-      <br />
-      <br />
       <div className='title'>
         <p className='title-style'>주일스케치</p>
         <p className='subtitle-style'>What's New</p>
       </div>
       <section className='videoSection'>
-        <article className='sermon_padding10'>
+        <article >
           <div className='whats_new_image_wrapper'>
             <div className=''>
               <img src={firstGallery.url} onClick={() => handleGallery(firstGallery)} className='whats_new_image' />
@@ -213,7 +182,7 @@ export default function Home() {
             </div>
           </div>
         </article>
-        <article className='sermon_padding10'>
+        <article >
           <div className='whats_new_image_wrapper'>
             <div className=''>
               <img src={secondGallery.url} onClick={() => handleGallery(secondGallery)} className='whats_new_image' />
@@ -223,7 +192,7 @@ export default function Home() {
             </div>
           </div>
         </article>
-        <article className='sermon_padding10'>
+        <article >
           <div className='whats_new_image_wrapper'>
             <div className=''>
               <img src={thirdGallery.url} onClick={() => handleGallery(thirdGallery)} className='whats_new_image' />
