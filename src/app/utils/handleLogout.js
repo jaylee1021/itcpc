992d843d1c73edb81ce7e2d418ac5b3f2b230c2c
Jay Lee <randomdori@gmail.com>
2023-11-27 23:23:36 -0800

    add removeitem username and userid

--- src/app/utils/handleLogout.js
+++ src/app/utils/handleLogout.js
@@ -1,10 +1,12 @@
 const handleLogout = () => {
-    if (localStorage.getItem('jwtToken')) {
-      // remove token for localStorage
-      localStorage.removeItem('jwtToken');
-      localStorage.removeItem('email');
-      localStorage.removeItem('expiration');
-    }
+  if (localStorage.getItem('jwtToken')) {
+    // remove token for localStorage
+    localStorage.removeItem('jwtToken');
+    localStorage.removeItem('email');
+    localStorage.removeItem('userName');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('expiration');
   }
+};
 
 export default handleLogout;
