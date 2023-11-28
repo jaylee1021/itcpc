2fa86628d3f8e28f407c21c5c1ca48b0a743fc9d
Jay Lee <randomdori@gmail.com>
2023-11-27 23:23:15 -0800

    remove error screen

--- src/app/users/login/page.js
+++ src/app/users/login/page.js
@@ -8,63 +8,39 @@ import jwtDecode from 'jwt-decode';
 export default function Login() {
     const router = useRouter();
     const [redirect, setRedirect] = useState(false);
-    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState(false);
 
-    const handleEmail = (e) => {
-        // fill in code
-        setEmail(e.target.value);
+    const handleUserName = (e) => {
+        setUserName(e.target.value);
     };
 
     const handlePassword = (e) => {
-        // fill in code
         setPassword(e.target.value);
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault(); // at the beginning of a submit function
+        e.preventDefault(); // prevents page from refreshing
 
-        axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/login`, { email, password })
+        axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/login`, { userName, password })
             .then(response => {
                 if (typeof window !== 'undefined') {
                     localStorage.setItem('jwtToken', response.data.token);
                     localStorage.setItem('email', response.data.userData.email);
+                    localStorage.setItem('userName', response.data.userData.userName);
                     localStorage.setItem('expiration', response.data.userData.exp);
                 }
                 setAuthToken(response.data.token);
-                let decoded = jwtDecode(response.data.token);
-                setRedirect(true);
+                // let decoded = jwtDecode(response.data.token);
+                // setRedirect(true);
+                router.push('/admin');
             })
             .catch(error => {
-                if (error.response.data.message === 'Email already exists') {
-                    console.log('===> Error in Signup', error.response.data.message);
-                    setError(true);
-                }
+                alert('Either your username or password is incorrect');
             });
-
     };
 
-    if (redirect) { router.push('/users/profile'); }
-    if (error) {
-        return (
-            <div>
-                <div className="card text-white bg-primary py-5 d-md-down-none" style={{ width: "44%" }}>
-                    <div className="card-body text-center">
-                        <div>
-                            <p>Email already exists</p>
-                            <br />
-                            <h2>Login</h2>
-                            <p>Sign In to your account</p>
-                            <a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
-                            <span>  </span>
-                            <a href="/users/signup" type="button" className="btn btn-secondary active mt-3">Signup</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    // if (redirect) { router.push('/admin'); }
 
     return (
         <div className="container">
@@ -76,16 +52,16 @@ export default function Login() {
                 <div className="col-md-8">
                     <div className="card-group mb-0">
                         <div className="card p-4">
-                            <form className="card-body" onSubmit={handleSubmit}>
-                                <h1>Login</h1>
-                                <p className="text-muted">Sign In to your account</p>
+                            <form className="card-body" id='form' onSubmit={handleSubmit}>
+                                <h1>Admin Login</h1>
+                                <p className="text-muted">Sign In to admin account</p>
                                 <div className="input-group mb-3">
                                     <span className="input-group-addon"><i className="fa fa-user"></i></span>
-                                    <input type="text" className="form-control" placeholder="Email" value={email} onChange={handleEmail} required />
+                                    <input type="text" name='username' className="form-control" placeholder="Username" value={userName} onChange={handleUserName} autoComplete='on' required />
                                 </div>
                                 <div className="input-group mb-4">
                                     <span className="input-group-addon"><i className="fa fa-lock"></i></span>
-                                    <input type="password" className="form-control" placeholder="Password" alue={password} onChange={handlePassword} required />
+                                    <input type="password" name='password' className="form-control" placeholder="Password" alue={password} onChange={handlePassword} required />
                                 </div>
                                 <div className="row">
                                     <div className="col-6">
@@ -96,15 +72,6 @@ export default function Login() {
                                     </div>
                                 </div>
                             </form>
-                        </div>
-                        <div className="card text-white bg-primary py-5 d-md-down-none" style={{ width: "44%" }}>
-                            <div className="card-body text-center">
-                                <div>
-                                    <h2>Sign up</h2>
-                                    <p>Get started now by creating an account.</p>
-                                    <a href="/users/signup" type="button" className="btn btn-primary active mt-3">Register Now!</a>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
