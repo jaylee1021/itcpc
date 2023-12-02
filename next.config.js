080a983572c013a149d9040abd14af4ae9f4aa1a
Jay Lee <leejayjong@gmail.com>
2023-12-01 21:04:20 -0800

    feat: add react-pdf

--- next.config.js
+++ next.config.js
@@ -30,6 +30,15 @@
 // module.exports = nextConfig;
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    future: { webpack5: true },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.resolve.alias.canvas = false;
+        config.resolve.alias.encoding = false;
+        return config;
+    }
+};
 module.exports = nextConfig;
