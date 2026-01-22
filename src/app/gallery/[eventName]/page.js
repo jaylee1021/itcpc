106a7914c0fc95d1578c3a31fc2c27959319b38c
Jay Lee <leejayjong@gmail.com>
2026-01-21 18:18:59 -0800

    remove wrangler and edge runtime

--- src/app/gallery/[eventName]/page.js
+++ src/app/gallery/[eventName]/page.js
@@ -5,8 +5,6 @@ import GetEventPhotos from '@/app/Component/GetEventPhotos';
 import '../../css/page.css';
 import '../../css/gallery.css';
 
-export const runtime = 'edge';
-
 export default function Gallery() {
 
     const { eventName } = useParams();
